869e9fa2e099f923720883e3c9f1b96e1dab252b
ajetty <aejetty@gmail.com>
2019-12-11 23:52:13 -0800

    fixed binding for Main2Activity/AndroidManifest file

--- app/src/main/java/com/example/weatherbuddy/ColorChooser.kt
+++ app/src/main/java/com/example/weatherbuddy/ColorChooser.kt
@@ -22,7 +22,7 @@ https://developer.android.com/reference/android/graphics/Color.html#parseColor(j
 A class that chooses and returns the appropriate color for the Weather Buddy image (MyImageView). Inputs being
 current temperature, humidity, and weather condition.
 
-The reason that we are using String, Int for ArrayMap is that we receive strings from the weather API and store colors as integers.
+The reason that we are using String, String for ArrayMap is that we receive strings from the weather API and store colors as integers.
 Color.parseColor takes the hex string value and gives RGB Alpha values from 0 to 254.
  */
 
@@ -78,11 +78,47 @@ class ColorChooser {
     }
 
     public fun getHumidityColor(value: String): String {
-        return colorMap[value]!!
+
+        var temp = value.toInt()
+
+        if(temp >= 100){
+            temp = 100
+        }
+        else if(temp in 99..97){
+            temp = 97
+        }
+        else if(temp in 96..94){
+            temp = 94
+        }
+        else if(temp in 93..85){
+            temp = 85
+        }
+        else if(temp in 84..75){
+            temp = 75
+        }
+        else if(temp in 74..69){
+            temp = 69
+        }
+        else if(temp in 68..58){
+            temp = 58
+        }
+        else if(temp in 57..53){
+            temp = 53
+        }
+        else if(temp in 52..33){
+            temp = 33
+        }
+        else if(temp in 32..11){
+            temp = 11
+        }
+        else
+            temp = 0
+
+        return colorMap[temp.toString()]!!
     }
 
     private fun setTemperatureColors(temperature: Array<String>) {
-        val idList = listOf("-114F", "-100F", "-50F", "-31F", "-25F", "0F", "20F", "30F", "40F", "50F", "60F",
+        val idList = listOf("-114F", "-100F", "-50F", "-32F", "-25F", "0F", "20F", "30F", "40F", "50F", "60F",
              "70F", "80F", "90F", "100F", "110F", "120F")
 
         for ((index, value) in idList.withIndex()){
@@ -94,7 +130,62 @@ class ColorChooser {
     }
 
     public fun getTemperatureColor(value: String): String {
-        return colorMap[value]!!
+
+        var temp = value.toInt()
+
+        if(temp <= -114){
+            temp = -114
+        }
+        else if(temp in -113..-100){
+            temp = -100
+        }
+        else if(temp in -99..-50){
+            temp = -50
+        }
+        else if(temp in -49..-32){
+            temp = -32
+        }
+        else if(temp in -31..-25){
+            temp = -25
+        }
+        else if(temp in -24..0){
+            temp = -0
+        }
+        else if(temp in 1..20){
+            temp = 20
+        }
+        else if(temp in 21..30){
+            temp = 30
+        }
+        else if(temp in 31..40){
+            temp = 40
+        }
+        else if(temp in 41..50){
+            temp = 50
+        }
+        else if(temp in 51..60){
+            temp = 60
+        }
+        else if(temp in 61..70){
+            temp = 70
+        }
+        else if(temp in 71..80){
+            temp = 80
+        }
+        else if(temp in 81..90){
+            temp = 90
+        }
+        else if(temp in 91..100){
+            temp = 100
+        }
+        else if(temp in 101..110){
+            temp = 110
+        }
+        else
+            temp = 120
+
+
+        return colorMap[temp.toString()]!!
     }
 
 }
